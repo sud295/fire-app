4801c340bde893d3ac15ae845e35ff7f1276cc07
shruti-create <shruti.bhamidipati@gmail.com>
2023-10-07 14:15:57 -0500

    initial commit

---
+++ react-app/App.js
@@ -0,0 +1,15 @@
+import logo from './logo.svg';
+import './App.css';
+import Map from './Map'
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <Map/>
+      </header>
+    </div>
+  );
+}
+
+export default App;
