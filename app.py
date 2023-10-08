18e6efe4a19bd313feca5f6d45a513f2cb9ced7e
sud295 <sonicsid12@gmail.com>
2023-10-08 10:35:44 -0500

    changes

--- app.py
+++ app.py
@@ -4,15 +4,30 @@ import csv
 from io import StringIO
 import yaml
 from flask_cors import CORS
+import time
+from datetime import datetime, timedelta  # Add this import
 
 app = Flask(__name__)
 CORS(app)
 
+api_data = None
+last_api_request_time = 0
+
 def get_api_data():
+    global api_data, last_api_request_time
+    current_time = time.time()
+
+    if current_time - last_api_request_time < 600:
+        return api_data
+    
+    print("Fetching Data...")
     with open("keys.yml", 'r') as f:
         data = yaml.safe_load(f)
         api_key = data.get('apiKey')
-    api_url = f'https://firms.modaps.eosdis.nasa.gov/api/area/csv/{api_key}/VIIRS_SNPP_NRT/world/1/2023-10-07'
+
+    current_date = (datetime.now() - timedelta(days=1)).strftime("%Y-%m-%d")
+
+    api_url = f'https://firms.modaps.eosdis.nasa.gov/api/country/csv/{api_key}/VIIRS_SNPP_NRT/USA/1/{current_date}'
     response = requests.get(api_url)
     
     if response.status_code == 200:
@@ -29,6 +44,9 @@ def get_api_data():
             }
             data_list.append(filtered_data)
         
+        api_data = data_list
+        last_api_request_time = current_time
+        
         return data_list
     else:
         return None
@@ -37,7 +55,7 @@ def get_api_data():
 def fetch_and_display_data():
     api_data = get_api_data()
     
-    if api_data is not None:
+    if api_data:
         return jsonify(api_data)
     else:
         return 'Failed to fetch data from the API'
