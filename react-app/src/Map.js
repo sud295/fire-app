65c59d09056b1aa795ff0bed09c4126005eb2332
shruti-create <shruti.bhamidipati@gmail.com>
2023-10-07 22:53:07 -0500

    changes

--- react-app/src/Map.js
+++ react-app/src/Map.js
@@ -1,31 +1,47 @@
-import React from "react";
-import GoogleMapReact from 'google-map-react';
+import React from 'react';
+import { GoogleMap, LoadScript, Marker } from '@react-google-maps/api';
 
-const AnyReactComponent = ({ text }) => <div>{text}</div>;
+const containerStyle = {
+  width: '100vw',
+  height: '100vh'
+};
 
-export default function SimpleMap(){
-  const defaultProps = {
-    center: {
-      lat: 10.99835602,
-      lng: 77.01502627
-    },
-    zoom: 11
-  };
+const center = {
+  lat: 41.85,
+  lng: -87.65
+};
+const markers = [
+  { id: 1, lat: 41.85, lng: -87.65, label: "Marker 1" },
+  { id: 2, lat: 41.86, lng: -87.60, label: "Marker 2" },
+  { id: 3, lat: 41.84, lng: -87.70, label: "Marker 3" }
+];
+const redCircleSVG = `
+  <svg height="10" width="10" xmlns="http://www.w3.org/2000/svg">
+    <circle cx="5" cy="5" r="5" fill="red" stroke="red" stroke-width="1" />
+  </svg>
+`;
 
+const redCircleURL = `data:image/svg+xml,${encodeURIComponent(redCircleSVG)}`;
+
+function MapComponent() {
   return (
-    // Important! Always set the container height explicitly
-    <div style={{ height: '100vh', width: '100%' }}>
-      <GoogleMapReact
-        bootstrapURLKeys={{ key: "" }}
-        defaultCenter={defaultProps.center}
-        defaultZoom={defaultProps.zoom}
+    <LoadScript googleMapsApiKey="AIzaSyBXzXpZUndUB8rjRqYBHwCUYiaKJQp6obg">
+      <GoogleMap
+        mapContainerStyle={containerStyle}
+        center={center}
+        zoom={10}
       >
-        <AnyReactComponent
-          lat={59.955413}
-          lng={30.337844}
-          text="My Marker"
+      {markers.map(marker => (
+        <Marker
+          key={marker.id}
+          position={{ lat: marker.lat, lng: marker.lng }}
+          label={marker.label}
+          icon={redCircleURL}
         />
-      </GoogleMapReact>
-    </div>
+      ))}
+      </GoogleMap>
+    </LoadScript>
   );
 }
+
+export default MapComponent;
