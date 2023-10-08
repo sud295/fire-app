d44d41e33a844c89c6d823094bc99fb460743289
shruti-create <shruti.bhamidipati@gmail.com>
2023-10-08 00:09:47 -0500

    a change

--- react-app/src/Map.js
+++ react-app/src/Map.js
@@ -10,22 +10,25 @@ const center = {
   lat: 41.85,
   lng: -87.65
 };
-const markers = [
-  { id: 1, lat: 41.85, lng: -87.65, label: "Marker 1" },
-  { id: 2, lat: 41.86, lng: -87.60, label: "Marker 2" },
-  { id: 3, lat: 41.84, lng: -87.70, label: "Marker 3" }
-];
-const redCircleSVG = `
-  <svg height="10" width="10" xmlns="http://www.w3.org/2000/svg">
-    <circle cx="5" cy="5" r="5" fill="red" stroke="red" stroke-width="1" />
-  </svg>
-`;
 
-const redCircleURL = `data:image/svg+xml,${encodeURIComponent(redCircleSVG)}`;
+
+const fireStyle = {
+  cursor: 'pointer',
+  color: 'red',
+  fontSize: '30px',
+  fontWeight: 'bold',
+};
+
+const markers = [
+  { lat: 41.85, lng: -87.65},
+  { lat: 45.86, lng: -60.60},
+  { lat: 49.84, lng: -69.70}
+];
+
 
 function MapComponent() {
   return (
-    <LoadScript googleMapsApiKey="AIzaSyBXzXpZUndUB8rjRqYBHwCUYiaKJQp6obg">
+    <LoadScript googleMapsApiKey="APIK">
       <GoogleMap
         mapContainerStyle={containerStyle}
         center={center}
@@ -34,10 +37,7 @@ function MapComponent() {
        
       {markers.map(marker => (
         <Marker
-          key={marker.id}
           position={{ lat: marker.lat, lng: marker.lng }}
-          label={marker.label}
-          icon={redCircleURL}
         />
       ))}
       </GoogleMap>
